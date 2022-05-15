4a3331171fdcbf9c39f5f01f308038ae8933b902
minwook <yjg02211@naver.com>
2022-05-15 20:30:33 +0900

    0515 commit

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,20 +1,66 @@
- let key = "90ad618fa3d2f888905f0b844cbb97df";
-let base_url = "https://image.tmdb.org/t/p/w500";
+let key = "90ad618fa3d2f888905f0b844cbb97df";
+let posterBase_url = "https://image.tmdb.org/t/p/w500";
+let popularityURL = `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=kr-KR&page=1`;
+let list = ['영화 정보', '예매', '마이페이지', '상영관'];
 
-Authentication = {
-  "success": true,
-  "expires_at": "2016-08-26 17:04:39 UTC",
-  "request_token": "ff5c7eeb5a8870efe3cd7fc5c282cffd26800ecd"
-}
 
-function fetchMovie(page) {
-  let url = `http://files.tmdb.org/p/exports/movie_ids_05_12_2022.json.gz`;
-  fetch(url)
-      .then(res => res.json())
-      .then(function (res) {
-          console.log(res);
-      });
+fetch(popularityURL)
+  .then(response => response.json())
+  .then((res) => {
+    console.log(res);
+    // 데이터 위치 : res.results[n]....
+    // 제목 : original_title
+    // 인기도 : popularity
+    // 포스터 : poster_path
+    // 개봉일 : release_date
+    // 점수 : vote_average
+    // id : id
+    // backdrop_path : backdrop_path
+
+  
+    // body
+    for(let i = 0; i < 4; i++){
+      // poster 주소값
+      let poster = posterBase_url + res.results[i].poster_path;
+      
+      // img 태그 생성 및 속성 값 설정
+      let imgTag = document.createElement('img');
+      let aTag = document.createElement('a');
+      let pTag = document.getElementById('p');
+
+      aTag.appendChild(imgTag);
+      body.appendChild(aTag);
+      body.appendChild(pTag);
+
+      imgTag.setAttribute('width', '300');
+      aTag.href = '/src/main/webapp/do.html';
+      pTag.innerHTML = res.results[i].original_title;
+      imgTag.src = poster;
+      
+      
+      
+      
+
+      // 상세 설명 값을 위한 fetch
+      let movie_id = res.results[i].id;
+      let movie_title = res.results[i].original_title;
+      let detailsURL = `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=kr-KR&query=${movie_title}&page=1`;
+      fetch(detailsURL)
+        .then(res => res.json())
+        .then((res)=>{
+          console.log(res)
+          console.log(res.results[0].overview);
+          let p = document.getElementById('p');
+          
+        })
+        .catch(error => console.log(error))
     }
 
 
-    window.addEventListener('onLoad', fetchMovie());
+
+    
+  })
+  .catch(error => console.log(error));
+
+
+  
